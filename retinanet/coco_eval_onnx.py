de32476400cd846ba5c8c2bd982320f0c237d19f
G-venkatesh1 <venkatesh.guduru@multicorewareinc.com>
2024-03-11 00:38:27 +0530

    ka

--- retinanet/coco_eval_onnx.py
+++ retinanet/coco_eval_onnx.py
@@ -13,7 +13,10 @@ def evaluate_coco_onnx(dataset,onnx_path, threshold=0.05):
         # start collecting results
         results = []
         image_ids = []
+        c=0
         for index in range(len(dataset)):
+            if(c>10): break
+            c=c+1
             data = dataset[index]
             xscale = data['xscale']
             yscale = data['yscale']
@@ -24,60 +27,61 @@ def evaluate_coco_onnx(dataset,onnx_path, threshold=0.05):
             ort_inputs = {input_name: img_tensor}
             ort_outputs = ort_session.run(output_names, ort_inputs)
             scores, labels, boxes = ort_outputs
+            print(labels.shape,boxes.shape,scores.shape)
             # boxes /= scale
-            if boxes.shape[0] > 0:
-                # change to (x, y, w, h) (MS COCO standard)
-                boxes[:, 2] -= boxes[:, 0]
-                boxes[:, 3] -= boxes[:, 1]
+        #     if boxes.shape[0] > 0:
+        #         # change to (x, y, w, h) (MS COCO standard)
+        #         boxes[:, 2] -= boxes[:, 0]
+        #         boxes[:, 3] -= boxes[:, 1]
                 
-                # compute predicted labels and scores
-                #for box, score, label in zip(boxes[0], scores[0], labels[0]):
-                print('no of boxes are',boxes.shape[0])
-                c=0
-                print(labels.shape,boxes.shape,scores.shape)
-                for box_id in range(boxes.shape[0]):
-                    print('loop number is',box_id)
-                    score = float(scores[box_id])
-                    label = int(labels[box_id])
-                    box = boxes[box_id, :]
+        #         # compute predicted labels and scores
+        #         #for box, score, label in zip(boxes[0], scores[0], labels[0]):
+        #         print('no of boxes are',boxes.shape[0])
+        #         c=0
+        #         print(labels.shape,boxes.shape,scores.shape)
+        #         for box_id in range(boxes.shape[0]):
+        #             print('loop number is',box_id)
+        #             score = float(scores[box_id])
+        #             label = int(labels[box_id])
+        #             box = boxes[box_id, :]
 
-                    # scores are sorted, so we can break
-                    # if score < threshold:
-                        # break
+        #             # scores are sorted, so we can break
+        #             # if score < threshold:
+        #                 # break
 
-                    # append detection for each positively labeled class
-                    image_result = {
-                        'image_id'    : dataset.image_ids[index],
-                        'category_id' : dataset.label_to_coco_label(label),
-                        'score'       : float(score),
-                        'bbox'        : box.tolist(),
-                    }
+        #             # append detection for each positively labeled class
+        #             image_result = {
+        #                 'image_id'    : dataset.image_ids[index],
+        #                 'category_id' : dataset.label_to_coco_label(label),
+        #                 'score'       : float(score),
+        #                 'bbox'        : box.tolist(),
+        #             }
 
-                    # append detection to results
-                    results.append(image_result)
-            print('count is',c)
-            # append image to list of processed images
-            image_ids.append(dataset.image_ids[index])
+        #             # append detection to results
+        #             results.append(image_result)
+        #     print('count is',c)
+        #     # append image to list of processed images
+        #     image_ids.append(dataset.image_ids[index])
 
-            # print progress
-            print('{}/{}'.format(index, len(dataset)), end='\r')
+        #     # print progress
+        #     print('{}/{}'.format(index, len(dataset)), end='\r')
 
-        if not len(results):
-            return
+        # if not len(results):
+        #     return
 
-        # write output
-        json.dump(results, open('{}_bbox_results.json'.format(dataset.set_name), 'w'), indent=4)
+        # # write output
+        # json.dump(results, open('{}_bbox_results.json'.format(dataset.set_name), 'w'), indent=4)
 
-        # load results in COCO evaluation tool
-        coco_true = dataset.coco
-        coco_pred = coco_true.loadRes('{}_bbox_results.json'.format(dataset.set_name))
+        # # load results in COCO evaluation tool
+        # coco_true = dataset.coco
+        # coco_pred = coco_true.loadRes('{}_bbox_results.json'.format(dataset.set_name))
 
-        # run COCO evaluation
-        coco_eval = COCOeval(coco_true, coco_pred, 'bbox')
-        coco_eval.params.imgIds = image_ids
-        coco_eval.evaluate()
-        coco_eval.accumulate()
-        coco_eval.summarize()
+        # # run COCO evaluation
+        # coco_eval = COCOeval(coco_true, coco_pred, 'bbox')
+        # coco_eval.params.imgIds = image_ids
+        # coco_eval.evaluate()
+        # coco_eval.accumulate()
+        # coco_eval.summarize()
 
         # model.train()
 
